2baf2aab844c2707d66961b302511b21efe1dc01
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-25 23:42:32 -0600

    Added tweet model

---
+++ models/tweet.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+from models import User
+
+
+class Tweet(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256,
+    )
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: Optional[datetime] = Field(None)
+    by: User = Field(...)
