e96c5ba561c7ebc73ed31e3add54840f43b81051
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-26 23:42:37 -0600

    Added users and auth routers to app

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from routes import route_auth, route_users
+
 app = FastAPI()
 
 
 @app.get(path="/")
 def home():
     return {"Twitter API": "Working!"}
+
+
+app.include_router(route_auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(route_users.router, prefix="/users", tags=["Users"])
