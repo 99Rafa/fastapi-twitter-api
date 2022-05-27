abd12a888a820daf84f2582cf74327b785456f59
rafa-aguirre <rafael.aguirre.ro@gmail.com>
2022-05-27 00:00:33 -0600

    Added tweets router to app

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from routes import route_auth, route_users
+from routes import route_auth, route_tweets, route_users
 
 app = FastAPI()
 
@@ -12,3 +12,4 @@ def home():
 
 app.include_router(route_auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(route_users.router, prefix="/users", tags=["Users"])
+app.include_router(route_tweets.router, prefix="/tweets", tags=["Tweets"])
